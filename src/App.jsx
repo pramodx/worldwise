9d701a2c004bc25674137b9cff9f2a164b0d3d2d
Pramod Nair <pramodn@buildxact.com>
2024-08-16 23:24:43 +1000

    basic pages done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AppLayout from './pages/AppLayout';
 import Homepage from './pages/Homepage';
+import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
 import Pricing from './pages/Pricing';
 import Product from './pages/Product';
@@ -8,9 +10,11 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<Homepage />} />
         <Route path='product' element={<Product />} />
         <Route path='pricing' element={<Pricing />} />
-        <Route path='/' element={<Homepage />} />
+        <Route path='login' element={<Login />} />
+        <Route path='app' element={<AppLayout />} />
         <Route path='*' element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
